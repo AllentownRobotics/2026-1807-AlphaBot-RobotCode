c232fea3dac1795aea454814f86ef192ea2777d6
Alexander Morgan <Morgan.A.2028@ufrsd.us>
2026-02-07 14:53:55 -0500

    test
    Added documentation for the Constants class.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,6 +18,10 @@ import edu.wpi.first.math.util.Units;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
+
+
+
+
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
